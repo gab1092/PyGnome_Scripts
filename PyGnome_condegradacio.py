c9e9f24566f8170ec2ee8dda3bb93bc30590ab1c
Gabriela <64294928+gab1092@users.noreply.github.com>
2022-11-30 08:30:35 -0800

    Add files via upload

--- PyGnome_condegradacio.py
+++ PyGnome_condegradacio.py
@@ -13,76 +13,110 @@ Created on Tue Nov  1 10:46:03 2022
 
  
 import os
-
-
-
+from gnome.utilities.distributions import UniformDistribution
 from datetime import datetime, timedelta
-import gnome
 import gnome.scripting as gs
 from gnome.outputters import NetCDFOutput
-from gnome.weatherers import Evaporation, NaturalDispersion, Emulsification, Biodegradation
+from gnome.weatherers import Evaporation, NaturalDispersion, Emulsification, Biodegradation, Dissolution
 from gnome.environment import Water, Waves
-from gnome.movers import GridCurrentMover, GridWindMover
-from gnome.movers import RandomMover, RandomMover3D
+#from gnome.movers import c_GridCurrentMover, c_GridWindMover
+from gnome.movers import RandomMover3D, RiseVelocityMover
 
- 
+
+
 #%%
-anio=2021
+anio=2022
 
-start_time = datetime(anio, 1, 1, 0, 0, 0)
+start_time = datetime(anio, 1, 1, 12, 0, 0)
 ##cargando el archivo de costa 
 mymap = gs.MapFromBNA('/media/gabriela/WDBlue_2/CIGOM/GNOME/Costa.bna', refloat_halflife=1)
 ##cargando al modelo archivo de costa , viento etc
 model = gs.Model(start_time=start_time,
-                 duration=timedelta(days=10),
-                 time_step=90*10,
+                 duration=timedelta(days=4),
+                 time_step=gs.minutes(60),
                  map=mymap)
 
 
 #%% MOVERS
 #para un archivo netcdf:
-c_mover=GridCurrentMover('/media/gabriela/WDBlue_2/CIGOM/datos_NCOM3km/NCOM-GAMSEAS-DAILY-20210101.nc')
-model.movers += c_mover
-##para una lista de archivos y funciona de forma similar para viento
-file=gs.get_datafile(r'/media/gabriela/WDBlue_2/CIGOM/NCOM_3km.txt')
-c_mover = GridCurrentMover(file)
+# file=gs.get_datafile(r'/media/gabriela/WDBlue_2/CIGOM/NCOM_3km.txt')
+# c_mover =c_GridCurrentMover(files)
+# c_mover=PyCurrentMover(files)
+
+# mover = gs.PyGridCurrentMover(fidef list_full_paths(directory):
+
+    
+# def list_full_paths(directory):
+#     return [os.path.join(directory, file) for file in os.listdir(directory)]
+
+# files=(list_full_paths((r'/media/gabriela/WDBlue_2/CIGOM/hycom_4km/')))
+# files.reverse()
+
+
+from gnome.movers import PyCurrentMover, PyWindMover
+
+c_mover=PyCurrentMover.from_netCDF('/media/gabriela/WDBlue_2/CIGOM/HYCOM_combined.nc')
 model.movers += c_mover
 
 
-w_mover = GridWindMover('/media/gabriela/WDBlue_2/CIGOM/GNOME/NCEP/ncep_global_c651_b4fe_659c.nc')
+w_mover = PyWindMover.from_netCDF('/media/gabriela/WDBlue_2/CIGOM/GNOME/NCEP/ncep_global_c651_b4fe_659c.nc')
+
 model.movers += w_mover
 
-#%% wind object para waves, evap, etc. MIsmo archivo, diferente forma de cargarlo.
 
+#%% wind object para waves, evap, etc. MIsmo archivo, diferente forma de cargarlo.
 w_obj = gs.GridWind.from_netCDF('/media/gabriela/WDBlue_2/CIGOM/GNOME/NCEP/ncep_global_c651_b4fe_659c.nc')
-water = Water(300,35) #temperature in Kelvin, salinity in psu
+water = Water(temperature=300.0, salinity=35.0) #temperature in Kelvin, salinity in psu
 waves = Waves(wind=w_obj,water=water)
+
+model.environment += waves
 
 model.weatherers += Evaporation(wind=w_obj,water=water)
 model.weatherers += NaturalDispersion(waves=waves,water=water)
 model.weatherers += Emulsification(waves=waves)
 model.weatherers += Biodegradation(waves=waves)
+model.weatherers += Dissolution(waves,w_obj)
 
-random_mover = RandomMover(diffusion_coef=10000) 
-model.movers += random_mover
 
-random_mover_3d = RandomMover3D(vertical_diffusion_coef_above_ml=10,vertical_diffusion_coef_below_ml=0.2,\
-mixed_layer_depth=10) #diffusion coefficients in cm/s, MLD in meters
+
+random_mover = gs.RandomMover(diffusion_coef=100000, uncertain_factor=2) 
+model.movers += random_mover      
+
+
+random_mover_3d = RandomMover3D(vertical_diffusion_coef_above_ml=10,vertical_diffusion_coef_below_ml=0.22,
+                                horizontal_diffusion_coef_above_ml=100000,horizontal_diffusion_coef_below_ml=126,
+                                mixed_layer_depth=15,surface_is_allowed=True) # Default is False 
 model.movers += random_mover_3d
+
+rise_vel_mover = RiseVelocityMover()
+model.movers += rise_vel_mover
 
 #%% DATOS DEL DERRAME
 
+substance=gs.GnomeOil(filename='/media/gabriela/WDBlue_2/CIGOM/GNOME/alaska-north-slope_AD00020.json')
 
-release= gnome.spill.spill.surface_point_line_spill(substance='AD00730',
-                                     amount=10000,num_elements=2000, units='bbls',
-                                     release_time=start_time,
-                                     start_position=(-92.154299,19.378562,0),
-                                     end_release_time=start_time+timedelta(days=3))
+spill= gs.surface_point_line_spill(substance=substance, num_elements=1000, amount=1000, units='bbls',
+                                       release_time=start_time,start_position=(-92.154299,19.378562,0),
+                                      end_release_time=start_time+timedelta(days=3))
 
-model.spills += release
+# ud = UniformDistribution(10e-6,300e-6) #droplets in the range 10-300 microns
+# spill = gs.subsurface_plume_spill(num_elements=10000,
+#                                 start_position=(-94.0592,18.8572,20),
+#                                 release_time=start_time,
+#                                 distribution=ud,
+#                                 distribution_type='droplet_size',
+#                                 end_release_time= start_time + timedelta(days=3),
+#                                 amount=10000,
+#                                 substance=gs.GnomeOil(filename='/media/gabriela/WDBlue_2/CIGOM/GNOME/isthmus_AD00578.json'),
+#                                 units='bbl',
+#                                 name='My spill')
+# #filename='/media/gabriela/WDBlue_2/CIGOM/GNOME/maya-exxon_AD00734.json'
+
+model.spills += spill
 
 
 #%%para guardarlo
+
 
 
 os.chdir('/media/gabriela/WDBlue_2/CIGOM/GNOME/Resultados/')
